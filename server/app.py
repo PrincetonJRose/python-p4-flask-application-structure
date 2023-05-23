83bb27164840a139e45a4393a507032430f035e6
Princeton J. Rose <princetonjrose@gmail.com>
2023-05-23 17:42:29 -0400

    finished lab

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route( '/custom/<string:message>' )
+def custom ( message ) :
+    return f'<h1> Hello there! Here is your { message } </h1>'
+
+@app.route( '/words' )
+def words ( ) :
+    return ['Apple', 'number', 'world', 'Cap']
+
+if __name__ == '__main__' :
+    app.run( port = 3000, debug = True )
